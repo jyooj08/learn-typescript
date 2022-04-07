a8d6b78d6f076a42af9c8ce5cb7160dab911222e
jyooj08 <jyooj08@naver.com>
2022-04-07 11:30:30 +0900

    Lecture 7

--- why-ts/app.js
+++ why-ts/app.js
@@ -1,10 +1,10 @@
 // api url
-var url = 'https://jsonplaceholder.typicode.com/users/1';
+var url = "https://jsonplaceholder.typicode.com/users/1";
 
 // dom
-var username = document.querySelector('#username');
-var email = document.querySelector('#email');
-var address = document.querySelector('#address');
+var username = document.querySelector("#username");
+var email = document.querySelector("#email");
+var address = document.querySelector("#address");
 
 // user data
 var user = {};
@@ -13,9 +13,13 @@ function startApp() {
   axios
     .get(url)
     .then(function (response) {
-      console.log(response);
+      // console.log(response);
       user = response.data;
       // TODO: 이름, 이메일, 주소 표시하기
+      console.log(user);
+      username.innerText = user.name;
+      email.innerText = user.email;
+      address.innerText = user.address.street;
     })
     .catch(function (error) {
       console.log(error);
