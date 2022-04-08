a5381aa2f6fd810448e7f49d84fb7e8b3dce0e52
jyooj08 <jyooj08@naver.com>
2022-04-08 11:14:37 +0900

    Lecture 19

--- class-note/2_functions.ts
+++ class-note/2_functions.ts
@@ -9,3 +9,8 @@ function sum2(): number {
 function sum3(a: number, b: number): number {
   return a + b;
 }
+
+// 함수의 옵셔널 파라미터
+function log(a: string, b?: string, c?: string) {}
+log("a");
+log("a", "b");
