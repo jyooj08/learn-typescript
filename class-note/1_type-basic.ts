29d7d5448c7b9c201a5a5f1420992abe27bdc562
jyooj08 <jyooj08@naver.com>
2022-04-08 10:58:06 +0900

    Lecture 16

--- class-note/1_type-basic.ts
+++ class-note/1_type-basic.ts
@@ -1,9 +1,30 @@
 // js 문자열 선언
 let str = "hello";
 
-// ts 변수 선언
+// ts 문자열
 let str2: string = "hello";
+
+// ts 숫자
 let num: number = 10;
+
+// ts 배열
 let arr: Array<number> = [1, 2, 3];
 let heroes: Array<string> = ["Capt", "Thor", "Hulk"];
 let items: number[] = [1, 2, 3];
+
+// ts 튜플
+let address: [string, number] = ["hello", 10];
+
+// ts 객체
+let obj: object = {};
+let person: object = {
+  name: "carry",
+  age: 30,
+};
+let person2: { name: string; age: number } = {
+  name: "yujin",
+  age: 25,
+};
+
+// ts 진위값
+let show: boolean = true;
