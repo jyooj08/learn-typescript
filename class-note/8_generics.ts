3925c0abff6be2ac05bd2f8c3dc0a948b7328856
jyooj08 <jyooj08@naver.com>
2022-04-15 17:29:48 +0900

    Lecture 55

---
+++ class-note/8_generics.ts
@@ -0,0 +1,15 @@
+// function logText<T>(text: T): T {
+//   console.log(text);
+//   return text;
+// }
+// logText<string>('하이');
+
+function logText(text: string) {
+  console.log(text);
+  return text;
+}
+
+function logNumber(num: number) {
+  console.log(num);
+  return num;
+}
