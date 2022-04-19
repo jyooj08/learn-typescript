f516db38357d236ca684d70e68bc0f23e68230d4
jyooj08 <jyooj08@naver.com>
2022-04-19 18:38:58 +0900

    Lecture 59

--- class-note/8_generics.ts
+++ class-note/8_generics.ts
@@ -1,15 +1,23 @@
-// function logText<T>(text: T): T {
-//   console.log(text);
-//   return text;
-// }
-// logText<string>('하이');
-
-function logText(text: string) {
+function logText<T>(text: T): T {
   console.log(text);
   return text;
 }
 
-function logNumber(num: number) {
-  console.log(num);
-  return num;
+// 타입은 호출할 때 결정한다!
+logText<string>("하이");
+
+// function logText(text: string) {
+//   console.log(text);
+//   return text;
+// }
+
+// function logNumber(num: number) {
+//   console.log(num);
+//   return num;
+// }
+
+// 인터페이스에 제네릭을 선언하는 방법
+interface Dropdown<T> {
+  value: T;
+  selected: boolean;
 }
--- example/dropdown-generic.ts
+++ example/dropdown-generic.ts
@@ -1,17 +1,37 @@
-const emails = [
-  { value: 'naver.com', selected: true },
-  { value: 'gmail.com', selected: false },
-  { value: 'hanmail.net', selected: false },
+interface DropdownItem<T> {
+  value: T;
+  selected: boolean;
+}
+
+// interface Email {
+//   value: string;
+//   selected: boolean;
+// }
+
+const emails: DropdownItem<string>[] = [
+  { value: "naver.com", selected: true },
+  { value: "gmail.com", selected: false },
+  { value: "hanmail.net", selected: false },
 ];
 
-const numberOfProducts = [
+// interface ProductNumber {
+//   value: number;
+//   selected: boolean;
+// }
+
+// interface TrueFalse {
+//   value: boolean;
+//   selected: boolean;
+// }
+
+const numberOfProducts: DropdownItem<number>[] = [
   { value: 1, selected: true },
   { value: 2, selected: false },
   { value: 3, selected: false },
 ];
 
-function createDropdownItem(item) {
-  const option = document.createElement('option');
+function createDropdownItem(item: DropdownItem<string> | DropdownItem<number>) {
+  const option = document.createElement("option");
   option.value = item.value.toString();
   option.innerText = item.value.toString();
   option.selected = item.selected;
@@ -21,6 +41,6 @@ function createDropdownItem(item) {
 // NOTE: 이메일 드롭 다운 아이템 추가
 emails.forEach(function (email) {
   const item = createDropdownItem(email);
-  const selectTag = document.querySelector('#email-dropdown');
+  const selectTag = document.querySelector("#email-dropdown");
   selectTag.appendChild(item);
 });
