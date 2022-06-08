39f32310d76c0a4f67ec170db99e67e89ff4e7d7
jyooj08 <jyooj08@naver.com>
2022-06-08 17:26:17 +0900

    실전 6강

--- class-note/13_utility-type.ts
+++ class-note/13_utility-type.ts
@@ -6,11 +6,6 @@ interface Product {
   stock: number;
 }
 
-// 상품 목록을 받아오기 위한 API 함수
-function fetchProducts(): Promise<Product[]> {
-  // ...
-}
-
 // interface ProductDetail {
 //   id: number;
 //   name: string;
@@ -18,4 +13,49 @@ function fetchProducts(): Promise<Product[]> {
 // }
 
 type ShoppingItem = Pick<Product, "id" | "name" | "price">;
-function displayProductDetail(shoppingItem: ShoppingItem) {}
+
+// interface UpdateProduct {
+//   id?: number;
+//   name?: string;
+//   price?: number;
+//   brand?: string;
+//   stock?: number;
+// }
+
+type UpdateProduct = Partial<Product>;
+
+// 유틸리티 타입 구현하기 - Partial
+interface UserProfile {
+  username: string;
+  email: string;
+  profilePhotoUrl: string;
+}
+
+// 방법1
+interface UserProfileUpdate1 {
+  username?: string;
+  email?: string;
+  profilePhotoUrl?: string;
+}
+
+// 방법2
+type UserProfileUpdate2 = {
+  username?: UserProfile["username"];
+  email?: UserProfile["email"];
+  profilePhotoUrl?: UserProfile["profilePhotoUrl"];
+};
+
+// 방법3
+type UserProfileUpdate3 = {
+  [p in "username" | "email" | "profilePhotoUrl"]?: UserProfile[p];
+};
+
+// 방법4
+type UserProfileUpdate4 = {
+  [p in keyof UserProfile]?: UserProfile[p];
+};
+
+// 방법5
+type Subset<T> = {
+  [p in keyof T]?: T[p];
+};
